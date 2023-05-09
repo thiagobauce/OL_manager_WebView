ff39fd5a06f31df90f38abb0c9d82aa4be97ef7f
Thiago Bauce <thiedbauce@gmail.com>
2023-05-09 13:34:09 -0400

    firsrt commit

---
+++ OL_App/app/src/main/java/com/example/ol_app/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.ol_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webview);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+
+        webView.loadUrl("http://sistema.player.net.br:8182/#/");
+    }
+}
