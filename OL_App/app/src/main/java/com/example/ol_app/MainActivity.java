d19206698bc66d5449e15a7dedcee581c9bc20ce
Thiago Bauce <thiedbauce@gmail.com>
2023-05-09 17:33:49 -0400

    add something to make run

--- OL_App/app/src/main/java/com/example/ol_app/MainActivity.java
+++ OL_App/app/src/main/java/com/example/ol_app/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.ol_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.ActivityNotFoundException;
+import android.content.ClipData;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_FILE_CHOOSER = 1;
+    private static final int REQUEST_PERMISSION_FILE_UPLOAD = 3;
+    private ValueCallback<Uri[]> mFilePathCallback;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,8 +32,63 @@ public class MainActivity extends AppCompatActivity {
 
         webView.setWebViewClient(new WebViewClient());
         webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().getAllowFileAccess();
+        webView.getSettings().getAllowFileAccessFromFileURLs();
         webView.getSettings().setDomStorageEnabled(true);
 
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    if (ContextCompat.checkSelfPermission(MainActivity.this,
+                            Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(MainActivity.this,
+                                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_PERMISSION_FILE_UPLOAD);
+                        return false;
+                    }
+                    mFilePathCallback = filePathCallback;
+                    Intent intent = fileChooserParams.createIntent();
+                    try {
+                        startActivityForResult(intent, REQUEST_FILE_CHOOSER);
+                    } catch (ActivityNotFoundException e) {
+                        mFilePathCallback = null;
+                        Toast.makeText(MainActivity.this, "Erro ao abrir o seletor de arquivos.", Toast.LENGTH_LONG).show();
+                        return false;
+                    }
+                    return true;
+                } else {
+                    Toast.makeText(MainActivity.this, "A versão do Android deve ser superior a Lollipop para usar esta funcionalidade.", Toast.LENGTH_LONG).show();
+                    return false;
+                }
+            }
+        });
+
         webView.loadUrl("http://sistema.player.net.br:8182/#/");
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_FILE_CHOOSER) {
+            if (mFilePathCallback != null) {
+                Uri[] results = null;
+                if (resultCode == RESULT_OK) {
+                    if (data != null) {
+                        String dataString = data.getDataString();
+                        ClipData clipData = data.getClipData();
+                        if (clipData != null) {
+                            results = new Uri[clipData.getItemCount()];
+                            for (int i = 0; i < clipData.getItemCount(); i++) {
+                                results[i] = clipData.getItemAt(i).getUri();
+                            }
+                        } else if (dataString != null) {
+                            results = new Uri[]{Uri.parse(dataString)};
+                        }
+                    }
+                }
+                mFilePathCallback.onReceiveValue(results);
+                mFilePathCallback = null;
+            }
+        }
     }
 }
